78c0a63f9dd1552b2b5b5ecc82b26427b96adc3b
Noemie <154456092+nono-fp@users.noreply.github.com>
2025-05-30 13:36:13 +0200

    Add files via upload

---
+++ ECE 2025/27.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+"""EXERCICE1"""
+
+def verifie(tab):
+    for i in range(len(tab)-1):
+        if tab[i] > tab[i+1]:
+            return False
+    return True
+
+"""EXERCICE 2 """
+
+def depouille(urne):
+    '''prend en paramètre une liste de suffrages et renvoie un
+    dictionnaire avec le nombre de voix pour chaque candidat'''
+    resultat = {}
+    for bulletin in urne:
+        if bulletin in resultat:
+            resultat[bulletin] = resultat[bulletin] + 1
+        else:
+            resultat[bulletin] = 1
+    return resultat
+
+def vainqueurs(election):
+    '''prend en paramètre un dictionnaire non vide avec le nombre
+    ↪ de voix
+    pour chaque candidat et renvoie la liste des vainqueurs'''
+    nmax = 0
+    for candidat in election:
+        if election[candidat] > nmax :
+            nmax = election[candidat]
+    liste_finale = [ nom for nom in election if election[nom] == nmax ]
+    return liste_finale
